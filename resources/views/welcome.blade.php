07b9c12bd1aa54a4d77e072c823fa36939f9d67c
hashim.hossam <hashimhossam@outlook.com>
2019-10-09 19:42:48 +0200

    Update MySQL DB Configuration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
 	<br><hr>
 		 هذا الموقع لأغراض تعليمية. لا تضع عليه بيانات شخصية حقيقية. المطور غير مسؤول عن اي استخدام سيئ او في غير محله لهذا الموقع. ايضا سيتم الغاء البيانات من قاعدة البيانات الخاصة بالموقع بشكل متكرر. لان قاعدة البيانات صغيرة جدا.
 	<br><hr>
-	This site is for educational purposes. Do not submit real personal data into it. The developer is not responsible of any misuseuse of this site. also the supported database for this site is too small and data will be deleted frequently from it.
+	This site is for educational purposes. Do not submit real personal data into it. The developer is not responsible for any misuseuse of this site. also the supported database for this site is too small and data will be deleted frequently from it.
 <hr><br>
 	</div>
                 <div class="title m-b-md">
